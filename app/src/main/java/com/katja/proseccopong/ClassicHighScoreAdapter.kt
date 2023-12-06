c19496313f96bdf54a685b02cf84e2324feb1854
Matilda <lmatilda@live.se>
2023-12-06 02:56:48 +0100

    Hade lite problem med shared preferencces som jag bheöver felsöka mer anledningen varför det ej fungerar. Men har fixat så activity highscore-lista läser in, uppdaterar och visar Highscore för Classic mode. Dock ser designen lite tokig ut, går fort och ändra det, men har lite frågor varför den ser ut som den gör.

--- app/src/main/java/com/katja/proseccopong/ClassicHighScoreAdapter.kt
+++ app/src/main/java/com/katja/proseccopong/ClassicHighScoreAdapter.kt
@@ -1,12 +1,14 @@
 package com.katja.proseccopong
 
+import android.graphics.Typeface
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 
-class ClassicHighScoreAdapter(private val highScores: List<HighScoreItem>) : RecyclerView.Adapter<ClassicHighScoreAdapter.ViewHolder>() {
+class ClassicHighScoreAdapter(private val highScores: List<Score>) : RecyclerView.Adapter<ClassicHighScoreAdapter.ViewHolder>() {
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
         val rankTextView: TextView = view.findViewById(R.id.rankTextView)
         val scoreTextView: TextView = view.findViewById(R.id.scoreTextView)
@@ -25,10 +27,15 @@ class ClassicHighScoreAdapter(private val highScores: List<HighScoreItem>) : Rec
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val highScore = highScores[position]
+        val score = highScores[position]
 
-        holder.rankTextView.text = highScore.rank.toString()
-        holder.scoreTextView.text = highScore.score.toString()
-        holder.nameTextView.text = highScore.playerName
+        // Assuming rank is the position in the list (1-based)
+        holder.rankTextView.text = (position + 1).toString()
+        holder.scoreTextView.text = score.score.toString()
+        holder.nameTextView.text = score.name
+
     }
+
+
 }
+
--- app/src/main/java/com/katja/proseccopong/Score.kt
+++ app/src/main/java/com/katja/proseccopong/Score.kt
@@ -1,6 +1,17 @@
 package com.katja.proseccopong
 
 // Bool True for classic score and False for Prosecco score
-class Score(val name: String, val score: Int, val classic: Boolean) {
+class Score(val name: String, var score: Int, val classic: Boolean) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Score) return false
 
+        return name == other.name && classic == other.classic
+    }
+
+    override fun hashCode(): Int {
+        var result = name.hashCode()
+        result = 31 * result + classic.hashCode()
+        return result
+    }
 }
