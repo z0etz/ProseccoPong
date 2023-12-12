9d4e35df8a98249cf0cfbd6c01153940da31177c
HanadAhmedM <146730531+HanadAhmedM@users.noreply.github.com>
2023-12-12 09:35:07 +0100

    Gör så att highscore-listan kan rensas.

--- app/src/main/java/com/katja/proseccopong/ClassicHighscoreActivity.kt
+++ app/src/main/java/com/katja/proseccopong/ClassicHighscoreActivity.kt
@@ -12,11 +12,12 @@ class ClassicHighscoreActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityClassicHighscoreBinding
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityClassicHighscoreBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        val deleteScore = application as MyApplication
         val mainmenubutton: Button = binding.buttonMainMenu
 
         mainmenubutton.setOnClickListener {
@@ -31,5 +32,11 @@ class ClassicHighscoreActivity : AppCompatActivity() {
         val adapter = ClassicHighScoreAdapter(ScoreList.scoreList)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
+        val buttonDelete = binding.imgBtnDelete // Change this to your ImageButton ID
+        buttonDelete.setOnClickListener {
+            deleteScore.clearAllData()
+            recreate() // Återskapar den nuvarande aktiviteten
+        }
     }
+
 }
--- app/src/main/java/com/katja/proseccopong/MyApplication.kt
+++ app/src/main/java/com/katja/proseccopong/MyApplication.kt
@@ -17,4 +17,14 @@ class MyApplication : Application() {
             ScoreList.scoreList.addAll(Gson().fromJson(scoreListJson, object : TypeToken<List<Score>>() {}.type))
         }
     }
+    // Function to clear all data, including "score_list", from SharedPreferences
+    fun clearAllData() {
+        val sharedPreferences = getSharedPreferences("game_scores", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.clear() // This will remove all data from SharedPreferences for "game_scores"
+        editor.apply()
+
+        // Additionally, clear the ScoreList
+        ScoreList.scoreList.clear()
+    }
 }
