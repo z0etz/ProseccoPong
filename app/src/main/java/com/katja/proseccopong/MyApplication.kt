e00f4605e16d1dbfd69dbf567a71ab12c2bbe293
HanadAhmedM <146730531+HanadAhmedM@users.noreply.github.com>
2023-12-13 15:32:29 +0100

    Gör_så_att_highscore-listan_kan_rensas_

--- app/src/main/java/com/katja/proseccopong/MyApplication.kt
+++ app/src/main/java/com/katja/proseccopong/MyApplication.kt
@@ -16,5 +16,18 @@ class MyApplication : Application() {
         if (scoreListJson?.isNotEmpty() == true) {
             ScoreList.scoreList.addAll(Gson().fromJson(scoreListJson, object : TypeToken<List<Score>>() {}.type))
         }
+
+
+
+
+    }
+    fun clearAllData() {
+        val sharedPreferences = getSharedPreferences("game_scores", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.clear() // This will remove all data from SharedPreferences for "game_scores"
+        editor.apply()
+
+        // Additionally, clear the ScoreList
+        ScoreList.scoreList.clear()
     }
 }
