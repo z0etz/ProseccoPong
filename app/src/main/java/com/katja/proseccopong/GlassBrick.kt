a23eea6e86405c45d134274e0a3789f908ef9952
z0etz <katja@klahr.se>
2023-12-14 21:59:00 +0100

    Lagt in brickor.

--- app/src/main/java/com/katja/proseccopong/GlassBrick.kt
+++ app/src/main/java/com/katja/proseccopong/GlassBrick.kt
@@ -5,51 +5,44 @@ import android.graphics.Canvas
 import androidx.core.content.ContextCompat
 
 class GlassBrick(private val gameView: GameView,
-                 val context: Context,
-                 var offsetFromMiddleX: Float,
-                 var offsetFromTopY: Float, var rose: Boolean, var viewWidth: Float, var viewHeight: Float) {
+                 val context: Context, val name: String, var width: Int,
+                 val numberFromMiddleX: Int,
+                 val numberFromTopY: Int, var rose: Boolean, var viewWidth: Float, var viewHeight: Float) {
 
     var hasBeenHit = false
     var glassImage = ContextCompat.getDrawable(context, R.drawable.glas_prosecco)
-    val width = 50
-    val height = width * 3
+    var height = width * 3
     var rotationAngle = 0f
+    val topBrickLayoutOffset = 400
 
-    init {
-        if (!rose) {
-            val glassImage = ContextCompat.getDrawable(context, R.drawable.glas_prosecco)
-        } else {
-            val glassImage = ContextCompat.getDrawable(context, R.drawable.glas_rose)
-        }
-    }
-
-
-    // Draw image centered at posX and posY
     fun draw(canvas: Canvas) {
         // Choose drawable
         if(!hasBeenHit) {
             if (!rose) {
-                val glassImage = ContextCompat.getDrawable(context, R.drawable.glas_prosecco)
+                glassImage = ContextCompat.getDrawable(context, R.drawable.glas_prosecco)
             } else {
-                val glassImage = ContextCompat.getDrawable(context, R.drawable.glas_rose)
+                glassImage = ContextCompat.getDrawable(context, R.drawable.glas_rose)
             }
         }
         else {
-            val glassImage = ContextCompat.getDrawable(context, R.drawable.glas_tomt)
+            glassImage = ContextCompat.getDrawable(context, R.drawable.glas_tomt)
         }
         // Set position
-        glassImage?.setBounds(viewWidth.toInt() / 2 + offsetFromMiddleX.toInt() - width/2,
-            offsetFromTopY.toInt() - height/2,
-            viewWidth.toInt() / 2 + offsetFromMiddleX.toInt() + width/2,
-             offsetFromTopY.toInt() + height/2)
+        glassImage?.setBounds(viewWidth.toInt() / 2 + numberFromMiddleX * width + numberFromMiddleX * 10 - width/2,
+            numberFromTopY * height - height/2 + numberFromTopY * 10 + topBrickLayoutOffset,
+            viewWidth.toInt() / 2 + numberFromMiddleX * width + numberFromMiddleX * 10 + width/2,
+            numberFromTopY * height + height/2 + numberFromTopY * 10 + topBrickLayoutOffset)
 
         if(rotationAngle == 0f) {
             glassImage?.draw(canvas)
         }
+        // Rotation used once a brick is hit
         else {
+            val x = numberFromMiddleX.toFloat() * width + numberFromMiddleX * 10
+            val y = numberFromTopY.toFloat() * height + numberFromTopY * 10 + topBrickLayoutOffset
             glassImage?.let {
                 canvas.save()
-                canvas.rotate(rotationAngle, offsetFromMiddleX, offsetFromTopY)
+                canvas.rotate(rotationAngle, x, y)
                 it.draw(canvas)
                 canvas.restore()
             }
@@ -61,9 +54,12 @@ class GlassBrick(private val gameView: GameView,
         // TODO: Lägg till mer logik för vad som ska hända när bricka träffas, efter att bilden roterat ska brickan bl.a. försvinna så småningom.
     }
 
-    fun sufaceChanged(width: Float, height: Float){
-        viewWidth = width
-        viewHeight = height
+    fun sufaceChanged(inputWidth: Float, inputHeight: Float, imageWidth: Int){
+        viewWidth = inputWidth
+        viewHeight = inputHeight
+        width = imageWidth
+        height = imageWidth * 3
+
     }
 
 }
