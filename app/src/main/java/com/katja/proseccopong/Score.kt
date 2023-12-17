3225cc5ff4e826c5f91c3fa46ae706d14b1a698d
Matilda <lmatilda@live.se>
2023-12-18 00:18:01 +0100

    Nu har jag skapat tid och datum i recyclerview, dock behöver designen justeras. Avvaktade med desigen tills vi vet hur det ska se ut.

--- app/src/main/java/com/katja/proseccopong/Score.kt
+++ app/src/main/java/com/katja/proseccopong/Score.kt
@@ -1,7 +1,12 @@
 package com.katja.proseccopong
 
 // Bool True for classic score and False for Prosecco score
-class Score(val name: String, var score: Int, val classic: Boolean) {
+import java.util.*
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+
+class Score(val name: String, var score: Int, val classic: Boolean, val timestamp: Long = System.currentTimeMillis()) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other !is Score) return false
@@ -14,4 +19,22 @@ class Score(val name: String, var score: Int, val classic: Boolean) {
         result = 31 * result + classic.hashCode()
         return result
     }
+
+    // getFormattedDate-metoden returnerar tidstämpeln som en formaterad sträng.
+    // Använder det angivna datumformatet ("yyyy-MM-dd HH:mm:ss").
+    fun getFormattedDate(): String {
+        // Skapa en instans av Calendar-klassen som håller tid och datum.
+        val calendar = Calendar.getInstance()
+
+        // Sätt tidsstämpeln på kalendern till den som finns i objektet (timestamp är tidpunkten då poängen skapades).
+        calendar.timeInMillis = timestamp
+
+        // Skapa en SimpleDateFormat-instans för att formatera datumet som en sträng.
+        // Det angivna formatet är "yyyy-MM-dd HH:mm:ss".
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+
+        // Returnera tidstämpeln som en sträng med det specificerade datumformatet.
+        return dateFormat.format(calendar.time)
+    }
 }
+
