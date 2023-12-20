d1fd39211fb2af86494c0356b561bb8205fe6bdb
Matilda <lmatilda@live.se>
2023-12-20 09:19:50 +0100

    Uppdaterat på ny gren - Skapat poängräkning vid kollision. Just nu får användaren 1 poäng vid träff på processo glas och 2 poäng vid rose glas.

--- app/src/main/java/com/katja/proseccopong/MyApplication.kt
+++ app/src/main/java/com/katja/proseccopong/MyApplication.kt
@@ -10,6 +10,7 @@ class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
+
         // Initialisera ScoreList från SharedPreferences när appen startar
         val sharedPreferences = getSharedPreferences("game_scores", Context.MODE_PRIVATE)
         val scoreListJson = sharedPreferences.getString("score_list", "")
